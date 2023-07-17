8d13fdf042de8429b4b33709ccab1e97825cd4c8
KarlTred265 Tred <tred26559@gmail.com>
2023-07-17 16:16:15 +0000

    added ditaelslist

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,7 +9,8 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
-
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -32,7 +33,7 @@ class PostSerializer(serializers.ModelSerializer):
         user = self.context['request'].user
         if user.is_authenticated:
             like = Like.objects.filter(
-                owner = user, post = obj
+                owner=user, post=obj
             ).first()
             return like.id if like else None
         return None
@@ -42,8 +43,8 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter', 
-            'like_id',
+            'title', 'content', 'image', 'image_filter',
+            'like_id', 'likes_count', 'comments_count',
         ]
-        
+
         
