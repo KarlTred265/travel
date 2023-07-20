28d87a6667049da1174a23efe2a1c45365ae6b55
KarlTred265 Tred <tred26559@gmail.com>
2023-07-20 17:42:37 +0000

    added new key

--- travels/settings.py
+++ travels/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
- ALLOWED_HOSTS = ['localhost', 'travel-apis-89bd4494711c.herokuapp.com']
+ ALLOWED_HOSTS = ['localhost', 'travel.herokuapp.com']
 
 
 
