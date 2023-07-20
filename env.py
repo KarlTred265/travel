3a99939c26f855025037bcaa3ac13b30f71bb275
KarlTred265 Tred <tred26559@gmail.com>
2023-07-20 15:52:50 +0000

    added more settings

--- env.py
+++ env.py
@@ -1,5 +1,6 @@
 import os
 os.environ['CLOUDINARY_URL'] = 'cloudinary://198789893311866:JwT5M6MeI6uLXI713R8QRXMzVO4@dannj5p63'
 os.environ['SECRET_KEY'] = 'Key_123Lmn17bBn123j'
-# os.environ['DEV'] = '1'
+os.environ['DEV'] = '1'
 os.environ['DATABASE_URL'] = "postgres://fkcwvanx:TITyt1g1jXt01EVuv9CIZbU_9IfbJFeE@lucky.db.elephantsql.com/fkcwvanx"
+os.environ.setdefault("SECRET_KEY", "key123HLMn1jjLi")
